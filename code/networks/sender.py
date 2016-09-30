5af7fc394afa50551e093a31b3f0331c09cd2dbe
luckydonald <luckydonald@flutterb.at>
2016-09-30 02:46:45 +0200

    I did advanced stuff I don't remember because it is 02:46 am.

--- code/networks/sender.py
+++ code/networks/sender.py
@@ -25,19 +25,20 @@ def send_message(msg):
 
 def broadcast(message):
     from dockerus import ServiceInfos
+    if not isinstance(message, str):
+        raise TypeError("Parameter `message` is not type `str` but {type}: {msg}".format(type=type(message), msg=message))
     hosts = ServiceInfos().other_hostnames
     # msg = MSG_FORMAT.format(length=len(message), msg=message)
     message += "\n"
     msg = "ANSWER " + str(len(message)) + "\n" + message
-
+    logger.debug("Prepared sending to *:{port}:\n{msg}".format(port=NODE_PORT, msg=msg))
+    msg = bytes(msg, "utf-8")
     for node_host in hosts:
-        logger.debug("Prepared sending to {host}:{port}:\n{msg}".format(host=node_host, port=NODE_PORT, msg=msg))
-        msg = bytes(msg, "utf-8")
         sent = False
         while not sent:
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # UDP SOCK_DGRAM
-                    logger.debug("Sending to {host}:{port}:\n{msg}".format(host=node_host, port=NODE_PORT, msg=msg))
+                    logger.debug("Sending to {host}:{port}:".format(host=node_host, port=NODE_PORT))
                     sock.connect((node_host, NODE_PORT))
                     sock.sendall(msg)
                     sent = True
