f2ae23f9d2e8915bd50a3560753753c26cbe0318
luckydonald <luckydonald@flutterb.at>
2016-10-24 20:45:25 +0200

    [db_proxy] Updated database to use the type column as discriminator (to tell ponyorm what class the row is)

--- code/db_proxy/database.py
+++ code/db_proxy/database.py
@@ -16,11 +16,12 @@ SEQUENCE_TYPE = int
 
 
 class DBMessage(db.Entity):
-    type = orm.Required(MSG_TYPE_TYPE)
+    type = orm.Discriminator(MSG_TYPE_TYPE)
     sequence_no = orm.Required(SEQUENCE_TYPE)
     node = orm.Optional(NODE_TYPE)
     value = orm.Optional(VALUE_TYPE)
     leader = orm.Optional(NODE_TYPE)
+    _discriminator_ = 0
 
     def from_db(self):
         clazz = MSG_TYPE_CLASS_MAP[self.type]
@@ -28,88 +29,32 @@ class DBMessage(db.Entity):
         return clazz.from_dict(self.as_dict())
     # end def
 
-    def to_db(self, msg):
-        self._to_db_message(msg)
-        if hasattr(msg, "node"):
-            self._to_db_node(msg)
-        # end if
-
-        if hasattr(msg, "value"):
-            self._to_db_value(msg)
-        # end if
-        if hasattr(msg, "leader"):
-            self._to_db_leader(msg)
-        # end if
-    # end def
-
-    @orm.db_session
-    def _to_db_message(self, msg):
-        assert isinstance(msg, messages.Message)
-        self.type = msg.type
-        self.sequence_no = msg.sequence_no
-    # end def
-
-    @orm.db_session
-    def _to_db_node(self, msg):
-        self.node = msg.node
-        # end if
-
-    # end if
-
-    @orm.db_session
-    def _to_db_value(self, msg):
-        self.value = msg.value
-        # end if
-
-    # end if
-
-    @orm.db_session
-    def _to_db_leader(self, msg):
-        self.leader = msg.leader
-        # end if
-    # end if
-
-    def as_dict(self):
-        data = dict(type=self.type, sequence_no=self.sequence_no, )
-        return data
+    @classmethod
+    def to_db(cls, msg):
+        return cls(**msg.to_dict())
     # end def
 # end class
 
 
 class DBInitMessage(DBMessage):
+    _discriminator_ = INIT
+
     def from_db(self):
         return messages.InitMessage(sequence_no=self.sequence_no, node=self.node, value=self.value)
     # end def
 
-    def to_db(self, msg):
+    @classmethod
+    def to_db(cls, msg):
         assert isinstance(msg, messages.InitMessage)
-        self._to_db_message(msg)
-        self._to_db_node(msg)
-        self._to_db_value(msg)
+        return super().to_db(msg)
     # end def
 # end class
 
 
 class DBProposeMessage(DBMessage):
+    _discriminator_ = PROPOSE
     proposal = orm.Required(VALUE_TYPE)
-    value_store_json = orm.Required(str)  # json
-
-    @property
-    def value_store(self):
-        import json
-        return json.loads(self.value_store_json)
-    # end def
-
-    @value_store.setter
-    def value_store(self, val):
-        import json
-        self.value_store_json = json.dumps(val)
-    # end def
-
-    @value_store.deleter
-    def value_store(self):
-        del self.value_store_json
-    # end def
+    value_store = orm.Required(orm.Json)  # json
 
     def from_db(self):
         return messages.ProposeMessage(
@@ -118,25 +63,21 @@ class DBProposeMessage(DBMessage):
         )
     # end def
 
-    @orm.db_session
-    def to_db(self, msg):
+    @classmethod
+    def to_db(cls, msg):
         assert isinstance(msg, messages.ProposeMessage)
-        self._to_db_message(msg)
-        self._to_db_node(msg)
-        self._to_db_leader(msg)
-        self.proposal = msg.proposal
-        self.value_store = msg.value_store
+        return super().to_db(msg)
     # end def
 # end class
 
 
 class DBPrevoteMessage(DBMessage):
-    def to_db(self, msg):
+    _discriminator_ = PREVOTE
+
+    @classmethod
+    def to_db(cls, msg):
         assert isinstance(msg, messages.PrevoteMessage)
-        self._to_db_message(msg)
-        self._to_db_node(msg)
-        self._to_db_leader(msg)
-        self._to_db_value(msg)
+        return super().to_db(msg)
     # end def
 
     def from_db(self):
@@ -146,12 +87,12 @@ class DBPrevoteMessage(DBMessage):
 
 
 class DBVoteMessage(DBMessage):
-    def to_db(self, msg):
+    _discriminator_ = VOTE
+
+    @classmethod
+    def to_db(cls, msg):
         assert isinstance(msg, messages.VoteMessage)
-        self._to_db_message(msg)
-        self._to_db_node(msg)
-        self._to_db_leader(msg)
-        self._to_db_value(msg)
+        return super().to_db(msg)
     # end def
 
     def from_db(self):
@@ -177,11 +118,9 @@ def to_db(msg):
         msg = messages.Message.from_dict(msg)  # make a Message subclass first.
     assert isinstance(msg, messages.Message)
     db_msg_clazz = MSG_TYPE_CLASS_MAP[msg.type]  # Key error = not implemented yet.
-    logger.warn(db_msg_clazz)
     assert issubclass(db_msg_clazz, DBMessage)
-    db_msg = db_msg_clazz(type=1, sequence_no=msg.sequence_no)
-    db_msg.type = msg.type
-    db_msg.to_db(msg)
+    db_msg = db_msg_clazz.to_db(msg)
+    assert db_msg.type == msg.type
     return db_msg
 # end def
 
