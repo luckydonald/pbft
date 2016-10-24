70a024b34617e9410a1d3b1c04022410b348350f
luckydonald <luckydonald@flutterb.at>
2016-10-24 19:39:40 +0200

    [db_proxy] Database working.

--- code/db_proxy/database.py
+++ code/db_proxy/database.py
@@ -1,10 +1,12 @@
+from luckydonaldUtils.functions import deprecated
 from pony import orm
-
+import logging
 from node import messages
 from .env import POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASS, POSTGRES_DB
 
 from node.enums import INIT, LEADER_CHANGE, PROPOSE, PREVOTE, VOTE
 
+logger = logging.getLogger(__file__)
 db = orm.Database()
 
 VALUE_TYPE = float
@@ -12,70 +14,83 @@ MSG_TYPE_TYPE = int
 NODE_TYPE = int
 SEQUENCE_TYPE = int
 
-class DBMixin(object):
-    def to_db(self, msg):
-        pass
-    # end def
 
-class DBMessageMixin(DBMixin):
+class DBMessage(db.Entity):
     type = orm.Required(MSG_TYPE_TYPE)
     sequence_no = orm.Required(SEQUENCE_TYPE)
+    node = orm.Optional(NODE_TYPE)
+    value = orm.Optional(VALUE_TYPE)
+    leader = orm.Optional(NODE_TYPE)
 
     def from_db(self):
-        return messages.Message(type=self.type, sequence_no=self.sequence_no)
+        clazz = MSG_TYPE_CLASS_MAP[self.type]
+        assert issubclass(clazz, messages.Message)
+        return clazz.from_dict(self.as_dict())
     # end def
 
     def to_db(self, msg):
-        super().to_db(msg)
+        self._to_db_message(msg)
+        if hasattr(msg, "node"):
+            self._to_db_node(msg)
+        # end if
+
+        if hasattr(msg, "value"):
+            self._to_db_value(msg)
+        # end if
+        if hasattr(msg, "leader"):
+            self._to_db_leader(msg)
+        # end if
+    # end def
+
+    @orm.db_session
+    def _to_db_message(self, msg):
         assert isinstance(msg, messages.Message)
         self.type = msg.type
         self.sequence_no = msg.sequence_no
     # end def
+
+    @orm.db_session
+    def _to_db_node(self, msg):
+        self.node = msg.node
+        # end if
+
+    # end if
+
+    @orm.db_session
+    def _to_db_value(self, msg):
+        self.value = msg.value
+        # end if
+
+    # end if
+
+    @orm.db_session
+    def _to_db_leader(self, msg):
+        self.leader = msg.leader
+        # end if
+    # end if
+
+    def as_dict(self):
+        data = dict(type=self.type, sequence_no=self.sequence_no, )
+        return data
+    # end def
 # end class
 
 
-class DBNodeMixin(DBMixin):
-    node = orm.Required(NODE_TYPE)
-
-    def to_db(self, msg):
-        super().to_db(msg)
-        self.node = msg.node
-    # end def
-# end def
-
-
-class DBValueMixin(DBMixin):
-    value = orm.Required(VALUE_TYPE)
-
-    def to_db(self, msg):
-        super().to_db(msg)
-        self.value = msg.value
-    # end def
-# end def
-
-
-class DBLeaderMixin(DBMixin):
-    leader = orm.Required(NODE_TYPE)
-
-    def to_db(self, msg):
-        super().to_db(msg)
-        self.leader = msg.leader
-    # end def
-# pass
-
-
-class DBInitMessage(db.Entity, DBMessageMixin, DBValueMixin):
+class DBInitMessage(DBMessage):
     def from_db(self):
         return messages.InitMessage(sequence_no=self.sequence_no, node=self.node, value=self.value)
     # end def
 
     def to_db(self, msg):
-        super().to_db(msg)
+        assert isinstance(msg, messages.InitMessage)
+        self._to_db_message(msg)
+        self._to_db_node(msg)
+        self._to_db_value(msg)
     # end def
 # end class
 
 
-class DBProposeMessage(db.Entity, DBMessageMixin, DBLeaderMixin):
+class DBProposeMessage(DBMessage):
     proposal = orm.Required(VALUE_TYPE)
     value_store_json = orm.Required(str)  # json
 
@@ -103,20 +118,25 @@ class DBProposeMessage(db.Entity, DBMessageMixin, DBLeaderMixin):
         )
     # end def
 
+    @orm.db_session
     def to_db(self, msg):
         assert isinstance(msg, messages.ProposeMessage)
-        super().to_db(msg)
+        self._to_db_message(msg)
+        self._to_db_node(msg)
+        self._to_db_leader(msg)
         self.proposal = msg.proposal
         self.value_store = msg.value_store
     # end def
 # end class
 
 
-class DBPrevoteMessage(db.Entity, DBMessageMixin, DBLeaderMixin, DBValueMixin):
+class DBPrevoteMessage(DBMessage):
     def to_db(self, msg):
         assert isinstance(msg, messages.PrevoteMessage)
-        super().to_db(msg)
-        self.value = msg.value
+        self._to_db_message(msg)
+        self._to_db_node(msg)
+        self._to_db_leader(msg)
+        self._to_db_value(msg)
     # end def
 
     def from_db(self):
@@ -125,13 +145,13 @@ class DBPrevoteMessage(db.Entity, DBMessageMixin, DBLeaderMixin, DBValueMixin):
 # end class
 
 
-class DBVoteMessage(db.Entity, DBMessageMixin, DBLeaderMixin, DBValueMixin):
-    value = orm.Required(VALUE_TYPE)
-
+class DBVoteMessage(DBMessage):
     def to_db(self, msg):
         assert isinstance(msg, messages.VoteMessage)
-        super().to_db(msg)
-        self.value = msg.value
+        self._to_db_message(msg)
+        self._to_db_node(msg)
+        self._to_db_leader(msg)
+        self._to_db_value(msg)
     # end def
 
     def from_db(self):
@@ -147,6 +167,7 @@ MSG_TYPE_CLASS_MAP = {
     VOTE: DBVoteMessage,
 }
 
+
 @orm.db_session
 def to_db(msg):
     if isinstance(msg, dict):
@@ -154,8 +175,10 @@ def to_db(msg):
         msg = messages.Message.from_dict(msg)  # make a Message subclass first.
     assert isinstance(msg, messages.Message)
     db_msg_clazz = MSG_TYPE_CLASS_MAP[msg.type]  # Key error = not implemented yet.
-    assert issubclass(db_msg_clazz, DBMessageMixin)
-    db_msg = db_msg_clazz()
+    logger.warn(db_msg_clazz)
+    assert issubclass(db_msg_clazz, DBMessage)
+    db_msg = db_msg_clazz(type=1, sequence_no=msg.sequence_no)
+    db_msg.type = msg.type
     db_msg.to_db(msg)
     return db_msg
 # end def
