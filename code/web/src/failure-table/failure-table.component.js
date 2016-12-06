c672fc2cb94ac1428beaec0c659e12179602fc5a
PlayingBacon <PlayingBacon@users.noreply.github.com>
2016-12-06 17:38:28 +0100

    [web] First sorta kinda prototype of a timeline. Sorta. Not working with real data.

--- code/web/src/failure-table/failure-table.component.js
+++ code/web/src/failure-table/failure-table.component.js
@@ -5,4 +5,165 @@
 
 angular.
     module('failureTable').
-    component('failureTable', {});
+    component('failureTable', {
+        templateUrl: 'failure-table/failure-table.template.html',
+        controller: ['$http', function FailureTableController($http) {
+            var self = this;
+
+            self.nodes = [{
+                "id": "1"    
+            }, {
+                "id": "2"
+            }, {
+                "id": "3"
+            }, {
+                "id": "4"
+            }/*, {
+                "id": "5"
+            }, {
+                "id": "6"
+            }*/];
+
+            self.setupTimeline = (function(data, help) {
+                d3.select("div#timeline").select("*").remove();
+
+                var svg = d3.select("div#timeline")
+                    .append("svg")
+                    .attr("width","100%").attr("height","100%");
+
+                var tl = d3.select("div#timeline")[0][0];
+                var width = tl.offsetWidth;
+                var height = tl.offsetHeight;
+
+                window.onresize = (function() {
+                    width = tl.offsetWidth;
+                    height = tl.offsetHeight;
+                    svg.selectAll("*").remove();
+                    setupBackground(svg,width,height);
+                    if (help) {
+                        setupHelpMeasurements(svg,width,height);
+                    }
+                    setupNodeElements(svg,width,height);
+                });
+                setupBackground(svg,width,height);
+                if (help) {
+                    setupHelpMeasurements(svg,width,height);
+                }
+                setupNodeElements(svg,width,height);
+            });
+
+            self.setupTimeline(null,false);
+            
+            function setupBackground(svg,svgWidth,svgHeight) {
+                svg.append("rect")
+                    .attr("x",0).attr("y",0)
+                    .attr("width",function(){return svgWidth;}).attr("height",function(){return svgHeight;})
+                    .attr("fill","white");
+            }
+
+            function setupNodeElements(svg, svgWidth, svgHeight) {
+                var len = self.nodes.length;
+                var nW = svgWidth/(len*len); // node width
+                var nH = 50; // node height
+                var nC = "red"; // node color
+                var gap = (svgWidth-(nW*len))/(len+1);  // one gap := (<width of svg> - <width of all nodes combined>) / (<number of gaps present in the svg>)
+                                                        // NOTE: every node has one gap to it's left and right
+                var y = svgHeight*0.1;
+                for (var i = 0; i < len; i++) {
+                    var x = i*nW+(i+1)*gap;
+                    svg.append("rect")
+                        .attr("x",x).attr("y",y)
+                        .attr("width",function(){return nW;}).attr("height",function(){return nH;})
+                        .attr("fill",nC);
+                    svg.append("text")
+                        .text(function(){return "Node " +self.nodes[i].id;})
+                        .attr("x",x+(nW/2)).attr("y",(y+10))
+                        .attr("fill","white")
+                        .attr("font-family","Verdana").attr("font-size","12px");
+                    svg.append("line")
+                        .attr("x1",(x+(nW/2))).attr("y1",y+(nH/2))
+                        .attr("x2",(x+(nW/2))).attr("y2",svgHeight)
+                        .attr("stroke",nC).attr("stroke-width",3).attr("stroke-linecap","round").attr("stroke-dasharray","1,10");
+                }
+                out("#!# SVG WIDTH " +(svgWidth)+ " :: WIDTH ALL NODES " +(nW*len)+ " :: WIDTH ALL GAPS " +(svgWidth-(nW*len))+ " :: WIDTH PER GAP " +((svgWidth-(nW*len))/(len+1)));
+            }
+
+            function setupHelpMeasurements(svg,svgWidth,svgHeight) {
+                // setup vertical line at svg middle
+                svg.append("line")
+                    .attr("x1",(svgWidth/2)).attr("y1",0)
+                    .attr("x2",(svgWidth/2)).attr("y2",svgHeight)
+                    .attr("stroke","black").attr("stroke-width",2);
+
+                // setup horizontal line at svg relative top
+                svg.append("line")
+                    .attr("x1",0).attr("y1",(svgHeight*0.05))
+                    .attr("x2",svgWidth).attr("y2",(svgHeight*0.05))
+                    .attr("stroke","black").attr("stroke-width",2);
+
+                // add additional vertical lines with marking text to horizontal line
+                for (var i = 0; i < (svgWidth/200); i++) {
+                    svg.append("line")
+                        .attr("x1",(svgWidth/2-(i*100))).attr("y1",(svgHeight*0.05))    // lines to the left of the middle
+                        .attr("x2",(svgWidth/2-(i*100))).attr("y2",(svgHeight*0.06))    // "
+                        .attr("stroke","black").attr("stroke-width",2);
+                    svg.append("text")
+                        .text(i*100)
+                        .attr("x",(svgWidth/2-(i*100))).attr("y",(svgHeight*0.025))
+                        .attr("font-family","Verdana").attr("font-size","10px")
+                        .attr("fill","black");
+                }
+                for (var i = 1; i < (svgWidth/200); i++) {
+                    svg.append("line")
+                        .attr("x1",(svgWidth/2+(i*100))).attr("y1",(svgHeight*0.05))    // lines to the right of the middle
+                        .attr("x2",(svgWidth/2+(i*100))).attr("y2",(svgHeight*0.06))    // "
+                        .attr("stroke","black").attr("stroke-width",2);
+                    svg.append("text")
+                        .text(i*100)
+                        .attr("x",(svgWidth/2+(i*100))).attr("y",(svgHeight*0.025))
+                        .attr("font-family","Verdana").attr("font-size","10px")
+                        .attr("fill","black");
+                }
+
+                // and also setup this weird little thingie
+                // i*nW+(i+1)*gap
+                /*var len = self.nodes.length;
+                var nW = svgWidth/(len*4); // node width
+                var gap = (svgWidth-(nW*len))/(len+1);
+                for (var i = 0; i < len; i++) {
+                    var x = i*nW+(i+1)*gap;
+                    // i*nW ..
+                    svg.append("line")
+                        .attr("x1",(i*x)).attr("y1",(svgHeight*0.25))
+                        .attr("x2",(i*x)+(i*nW)).attr("y2",svgHeight*0.25)
+                        .attr("stroke","blue").attr("stroke-width",2);
+                    svg.append("line")
+                        .attr("x1",(i*x)).attr("y1",(svgHeight*0.25))
+                        .attr("x2",(i*x)).attr("y2",(svgHeight*0.3))
+                        .attr("stroke","blue").attr("stroke-width",1);
+                    svg.append("line")
+                        .attr("x1",(i*x)+(i*nW)).attr("y1",(svgHeight*0.25))
+                        .attr("x2",(i*x)+(i*nW)).attr("y2",(svgHeight*0.3))
+                        .attr("stroke","blue").attr("stroke-width",1);
+
+                    // + (i+1)*gap
+                    svg.append("line")
+                        .attr("x1",(i*x)+(i*nW)).attr("y1",(svgHeight*0.35))
+                        .attr("x2",(i*x)+(i*nW+(i+1)*gap)).attr("y2",svgHeight*0.35)
+                        .attr("stroke","orange").attr("stroke-width",2);
+                    svg.append("line")
+                        .attr("x1",(i*x)+(i*nW)).attr("y1",(svgHeight*0.35))
+                        .attr("x2",(i*x)+(i*nW)).attr("y2",(svgHeight*0.4))
+                        .attr("stroke","orange").attr("stroke-width",1);
+                    svg.append("line")
+                        .attr("x1",(i*x)+(i*nW+(i+1)*gap)).attr("y1",(svgHeight*0.35))
+                        .attr("x2",(i*x)+(i*nW+(i+1)*gap)).attr("y2",(svgHeight*0.4))
+                        .attr("stroke","orange").attr("stroke-width",1);
+                }*/
+            }
+
+            function out(str) {
+                console.log(str);
+            }
+        }]
+});
--- code/web/src/value-graph/value-graph.component.js
+++ code/web/src/value-graph/value-graph.component.js
@@ -11,17 +11,84 @@ angular.
             nodeid: '='
         },
         controller: ['$http', function ValueGraphController($http) {
-            /*function increment() {
-                this.id++;
-                console.log(this.id);
-            }
-            
-            function decrement() {
-                this.id--;
-                console.log(this.id);
+            var nodeData = null;
+            var self = this;
+
+            $http.get('node-values.json').then(function (response) {
+                nodeData = response.data;
+                var str = "### DATA :: ";
+                for (var i = 0; i < nodeData[self.nodeid].length; i++) {
+                    str = str+ "(" +i+ ")->" +nodeData[self.nodeid][i].value+ " ";
+                }
+                out(str);
+                constructVG(nodeData[self.nodeid]);
+            }, function(response) {
+                out("Yeah, that did not work, at all.");
+            }, function(response) {
+                out("What does this even do?");
+            });
+
+            out("data outer :: " +nodeData);
+
+            function constructVG(data) {
+                var svg = d3.select("div.value-graph")
+                    .append("svg")
+                    .attr("width", "100%")
+                    .attr("height", "100%")
+                    .attr("preserveAspectRatio","xMidYMin");
+
+                var vg = d3.select("div.value-graph")[0][0];
+                var max = data.length;
+                var width = vg.offsetWidth;
+                var height = vg.offsetHeight;
+                out("### MEASUREMENTS :: width " +width+ " height " +height+ " max " +max);
+
+                window.onresize = (function(){
+                    width = vg.offsetWidth;
+                    height = vg.offsetHeight;
+                    createSVGContent(svg,width,height,data,max);
+                    console.log("resize!");
+                });
+
+                /*var xScale = d3.scale.linear()
+                    .domain([0,d3.max()])
+                    .range();
+                var yScale = d3.scale.linear()
+                    .domain()
+                    .range();*/
+
+                createSVGContent(svg,width,height,data,max);
             }
 
-            this.increment = increment;
-            this.decrement = decrement;*/
+            function createSVGContent(svg, width, height, data, max) {
+                /*svg.selectAll("*").remove();
+                svg.append("rect").attr("width",width).attr("height",height).attr("fill","blue");
+                svg.selectAll("circle")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function(d,i){return i*(width/max)+((width/2)/max);})
+                    .attr("cy", function(){return height/2;})
+                    .attr("r", function(d){return d.value*30;})
+                    .attr("fill", "white");
+                */
+                var lineFunction = d3.svg.line()
+                    .x(function(d){return d.x;})
+                    .y(function(d){return d.y;})
+                    .interpolate("linear");
+
+                svg.selectAll("*").remove();
+                svg.append("rect").attr("width",width).attr("height",height).attr("fill","blue");
+                svg.append("path")
+                    .attr("d", lineFunction(data))
+                    .attr("stroke", "black")
+                    .attr("stroke-width", 2)
+                    .attr("fill", "none");
+            }
+
+            // I'm a lazy fuck.
+            function out(str) {
+                console.log(str);
+            }
         }]
     });
