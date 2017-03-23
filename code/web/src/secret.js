18259bf73b69602d6af0411b5ebe373ee5d90ff9
luckydonald <luckydonald@flutterb.at>
2017-03-23 16:37:00 +0100

    [web] secret.js for non-docker use.

---
+++ code/web/src/secret.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// WARNING
+// If you use docker, to start this
+// this file will be overwritten on startup.
+// This happens in entrypoint.sh,
+// _SECRET_URL is set to $JS_PATH.
+// WARNING
+
+var _SECRET_URL = "http://localhost";
